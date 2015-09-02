5048445bae0b6943dca7cdaeb9bc62f0ca3ff21f
zhangminglei <minglei.l.zhang@intel.com>
2015-09-02 18:20:15 +0800

    Some code

---
+++ src/BasicJava/TestString.java
@@ -0,0 +1,14 @@
+package BasicJava;
+
+/**
+ * A good question.
+ */
+public class TestString {
+  public static void main(String[] args) {
+    int i = 5;
+    String str = "a" + i;
+    System.out.println(str);
+    i = 10;
+    System.out.println(str);
+  }
+}
---
+++ src/BasicJava/TestVector.java
@@ -0,0 +1,23 @@
+package BasicJava;
+
+import java.util.Vector;
+
+/**
+ * Created by mingleiz on 8/31/2015.
+ */
+public class TestVector {
+
+  public static Object getLast(Vector list) {
+    int lastIndex = list.size() - 1;
+    return list.get(lastIndex);
+  }
+
+  public static void deleteLast(Vector list) {
+    int lastIndex = list.size() - 1;
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
